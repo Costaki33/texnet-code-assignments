a5596f3c0f949331f774278e43183de99547c21b
Costaki33 <cskevofilax@utexas.edu>
2024-06-09 17:00:07 -0500

    update, added user to provide EVENTS_DIR from CL

--- plot_events_location/plot_texnet_events.py
+++ plot_events_location/plot_texnet_events.py
@@ -1,35 +1,46 @@
 import os
+import argparse
 from obspy import read_events
 import matplotlib.pyplot as plt
 
-# Location of 'Events' folder to local file system
-EVENTS_DIR = '/home/skevofilaxc/Downloads/Events'
 
-# Lists to save latitudes and longitudes for plotting
-longitudes = []
-latitudes = []
+def main():
+    # Command-line parsing for user to provide EVENTS_DIR from CL argument
+    parser = argparse.ArgumentParser(description='Plot earthquake event locations from QML files from an EVENTS_DIR.')
+    parser.add_argument('EVENTS_DIR', type=str, help='Path to the directory containing the event files')
+    args = parser.parse_args()
 
-# Iterate through each .qml file in the 'Events' directory
-for event_file in os.listdir(EVENTS_DIR):
-    if event_file.endswith('.qml'):
-        file_path = os.path.join(EVENTS_DIR, event_file)
-        catalog = read_events(file_path)  # Read event file
+    EVENTS_DIR = args.EVENTS_DIR  # User provided EVENTS_DIR
 
-        # Extract longitude and latitude from event and append to list
-        for event in catalog:
-            origin = event.origins[0]
-            longitudes.append(origin.longitude)
-            latitudes.append(origin.latitude)
+    # Lists to save latitudes and longitudes for plotting
+    longitudes = []
+    latitudes = []
 
-# Plot event locations
-title = 'TexNet Earthquake Event Locations'
-fig = plt.figure(figsize=(10, 6))
-fig.canvas.manager.set_window_title(title + ' Plot')
+    # Iterate through each .qml file in the 'Events' directory
+    for event_file in os.listdir(EVENTS_DIR):
+        if event_file.endswith('.qml'):
+            file_path = os.path.join(EVENTS_DIR, event_file)
+            catalog = read_events(file_path)  # Read event file
 
-plt.scatter(longitudes, latitudes, c='orange', marker='o', edgecolor='k', s=100, alpha=0.7)
-plt.title(title)
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.grid(True)
-plt.savefig('texnet_earthquake_event_locations_plot.png')
-plt.show()
+            # Extract longitude and latitude from event and append to list
+            for event in catalog:
+                origin = event.origins[0]
+                longitudes.append(origin.longitude)
+                latitudes.append(origin.latitude)
+
+    # Plot event locations
+    title = 'TexNet Earthquake Event Locations'
+    fig = plt.figure(figsize=(10, 6))
+    fig.canvas.manager.set_window_title(title + ' Plot')
+
+    plt.scatter(longitudes, latitudes, c='orange', marker='o', edgecolor='k', s=100, alpha=0.7)
+    plt.title(title)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.grid(True)
+    plt.savefig('texnet_earthquake_event_locations_plot.png')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
