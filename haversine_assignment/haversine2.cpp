c5258b78f6346285bcdcdd3f1d715367ce3110d8
Costaki33 <cskevofilax@utexas.edu>
2024-06-10 14:23:18 -0500

    completed c++ code that allows users to provide points

--- haversine_assignment/haversine2.cpp
+++ haversine_assignment/haversine2.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
+#include <iomanip>
+#include <string> 
 #include <cmath>
 
-double deg_to_rad(double degree) {
+double convertDegreesToRadians(double degree) {
+    /**
+     * Function that converts degrees to radians
+     */
     return degree * M_PI / 180;
 }
 
 double haversine(double lat1, double lon1, double lat2, double lon2, std::string unit = "km") {
-    // Degrees to radians
-    lat1 = deg_to_rad(lat1);
-    lon1 = deg_to_rad(lon1);
-    lat2 = deg_to_rad(lat2);
-    lon2 = deg_to_rad(lon2);
+    /**
+    * Function that calculates the haversine distance between two points and returns the distance calculated 
+    **/
 
-    // Haversine formula
-    double dlon = lon2 - lon1;
-    double dlat = lat2 - lat1;
-    double a = std::pow(std::sin(dlat / 2), 2) + std::cos(lat1) * std::cos(lat2) * std::pow(std::sin(dlon / 2), 2);
+    lat1 = convertDegreesToRadians(lat1), lon1 = convertDegreesToRadians(lon1), lat2 = convertDegreesToRadians(lat2), lon2 = convertDegreesToRadians(lon2);  // Degrees to radians
+    double delta_lon = lon2 - lon1;
+    double delta_lat = lat2 - lat1;
+    
+    double a = std::pow(std::sin(delta_lat / 2), 2) + std::cos(lat1) * std::cos(lat2) * std::pow(std::sin(delta_lon / 2), 2);
     double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1 - a));
 
-    // Radius of the Earth
-    double r = 6371; // km
+    double radius = 6371; // km
     if (unit == "mi") {
-        r = 3956; // miles
+        radius = 3956; // miles
     }
 
-    double distance = r * c;
+    double distance = radius * c;
 
     return distance;
 }
 
 int main() {
-    // Sample
-    double lat1 = 51.5074, lon1 = -0.1278; // London, England
-    double lat2 = 48.8566, lon2 = 2.3522;  // Paris, France
+    double lat1, lon1, lat2, lon2;
+    std::string unit;
 
-    double distance_miles = haversine(lat1, lon1, lat2, lon2, "mi");
-    std::cout << "Distance: " << std::fixed << distance_miles << " miles" << std::endl;
+    std::cout << "Enter the first latitude and longitude point as lat1 lon1: ";
+    std::cin >> lat1 >> lon1;
+
+    std::cout << "Enter the second latitude and longitude point as lat2 lon2: ";
+    std::cin >> lat2 >> lon2;
+
+    std::cout << "Enter the unit you would like to use (km or mi): ";
+    std::cin >> unit;
+
+    double distance = haversine(lat1, lon1, lat2, lon2, unit);
+
+    // Round calculated distance for easier readability 
+    std::cout << "Calculated Haversine Distance: " << std::fixed << std::setprecision(2) << distance << " " << unit << std::endl;
 
     return 0;
 }
